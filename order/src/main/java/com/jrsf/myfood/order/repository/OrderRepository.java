f48bcd47eff6797b928bd527adfc7e8997bdb9f1
Roberto de Souza <rabbittrix@hotmail.com>
2020-10-09 00:46:17 +0100

    Created Repository

---
+++ order/src/main/java/com/jrsf/myfood/order/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.jrsf.myfood.order.repository;
+
+import com.jrsf.myfood.order.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
