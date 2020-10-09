dec6d4e78834b2602d5d0a8701608a475b21083a
Roberto de Souza <rabbittrix@hotmail.com>
2020-10-09 01:24:14 +0100

    Created Lombok

--- order/src/main/java/com/jrsf/myfood/order/entity/Order.java
+++ order/src/main/java/com/jrsf/myfood/order/entity/Order.java
@@ -1,17 +1,28 @@
 package com.jrsf.myfood.order.entity;
 
+import com.jrsf.myfood.order.dto.OrderDto;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.modelmapper.ModelMapper;
 
 import javax.persistence.*;
 
 @Data
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "tb_order")
 public class Order {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
     private Long idClient;
     private Long idRestaurant;
     private Long idMenu;
+
+    public static Order create(OrderDto orderDto){
+        return new ModelMapper().map(orderDto, Order.class);
+    }
 }
