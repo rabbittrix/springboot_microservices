31ebef572a8bed53b37a4971760d089eac8c700e
Roberto de Souza <rabbittrix@hotmail.com>
2020-10-09 01:23:51 +0100

    Created Lombok

---
+++ order/src/main/java/com/jrsf/myfood/order/controller/OrderController.java
@@ -0,0 +1,33 @@
+package com.jrsf.myfood.order.controller;
+
+import com.jrsf.myfood.order.dto.OrderDto;
+import com.jrsf.myfood.order.entity.Order;
+import com.jrsf.myfood.order.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @Autowired
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @PostMapping("/insert")
+    public ResponseEntity insertOrder(@RequestBody OrderDto orderDto){
+
+        return ResponseEntity.ok(orderService.insertOrder(
+                new Order(null,
+                orderDto.getIdClient(),
+                orderDto.getIdMenu(),
+                orderDto.getIdRestaurant())));
+    }
+}
