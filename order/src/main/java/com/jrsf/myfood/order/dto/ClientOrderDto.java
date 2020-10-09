ead89d81b5ac2c53a0ea8247025578eb02e035fe
Roberto de Souza <rabbittrix@hotmail.com>
2020-10-09 20:48:21 +0100

    Create ClientOrderDto in Order

---
+++ order/src/main/java/com/jrsf/myfood/order/dto/ClientOrderDto.java
@@ -0,0 +1,12 @@
+package com.jrsf.myfood.order.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClientOrderDto {
+    private Long idClient;
+}
