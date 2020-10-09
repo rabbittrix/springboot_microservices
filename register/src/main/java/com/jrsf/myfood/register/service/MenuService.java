2af4e1369f32c6dc35cff4a27b9d7890d7bf47cd
Roberto de Souza <rabbittrix@hotmail.com>
2020-10-10 00:42:54 +0100

    Implement new services RabbitMQ

--- register/src/main/java/com/jrsf/myfood/register/service/MenuService.java
+++ register/src/main/java/com/jrsf/myfood/register/service/MenuService.java
@@ -1,10 +1,13 @@
 package com.jrsf.myfood.register.service;
 
 import com.jrsf.myfood.register.dto.MenuDto;
+import com.jrsf.myfood.register.dto.MenuOrderDto;
 import com.jrsf.myfood.register.entity.Menu;
 import com.jrsf.myfood.register.entity.Restaurant;
+import com.jrsf.myfood.register.message.MenuSendMessage;
 import com.jrsf.myfood.register.repository.MenuRepository;
 import com.jrsf.myfood.register.repository.RestaurantRepository;
+import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,22 +18,31 @@ public class MenuService {
 
     private final MenuRepository menuRepository;
     private final RestaurantRepository restaurantRepository;
+    private final MenuSendMessage menuSendMessage;
 
     @Autowired
-    public MenuService(MenuRepository menuRepository, RestaurantRepository restaurantRepository) {
+    public MenuService(MenuRepository menuRepository, RestaurantRepository restaurantRepository, MenuSendMessage menuSendMessage) {
         this.menuRepository = menuRepository;
         this.restaurantRepository = restaurantRepository;
+        this.menuSendMessage = menuSendMessage;
     }
 
-    public Menu insertMenu(MenuDto menuDto){
-        Optional<Restaurant> restaurant = restaurantRepository.findById(menuDto.getRestaurant());
-        if (restaurant.isPresent()){
+    public Menu insertMenu(MenuDto menuDto) throws NotFoundException {
+
+        System.out.println(menuDto);
+
+        Optional<Restaurant> restaurant = restaurantRepository.findById(menuDto.getIdRestaurant());
+
+        if (restaurant.isPresent()) {
             Menu menu = Menu.create(menuDto);
             menu.setRestaurant(restaurant.get());
-            return menuRepository.save(menu);
-        }else {
-            return null;
+            Menu newMenu = menuRepository.save(menu);
+            menuSendMessage.sendMessage(new MenuOrderDto(newMenu.getId(), restaurant.get().getId()));
+            return newMenu;
         }
+
+        throw new NotFoundException("Restaurant not found");
+
     }
 
     public Menu updateMenu(Menu menu){
