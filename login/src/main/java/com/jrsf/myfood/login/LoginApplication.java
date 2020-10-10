8644abe3497ca84ed5c56af51f28afc7e0bb2700
Roberto de Souza <rabbittrix@hotmail.com>
2020-10-10 03:35:10 +0100

    update jwt

---
+++ login/src/main/java/com/jrsf/myfood/login/LoginApplication.java
@@ -0,0 +1,15 @@
+package com.jrsf.myfood.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class LoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginApplication.class, args);
+	}
+
+}
---
+++ login/src/main/java/com/jrsf/myfood/login/jwt/JwtUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.jrsf.myfood.login.jwt;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        return new User(username, "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6",
+                new ArrayList<>());
+
+    }
+
+}
